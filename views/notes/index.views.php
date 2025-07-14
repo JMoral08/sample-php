5320ac491b2025982a0b4ffbeb95503d17161419
Junjie Morales <act.jmorales@gmail.com>
2025-07-14 23:14:26 +0800

    New Changes

--- views/notes/index.views.php
+++ views/notes/index.views.php
@@ -9,8 +9,8 @@
         <a href="/note?id=<?= $note['id'] ?>" class="text-decoration-none">
             <div class="grid mb-2">
                 <div class="d-flex flex-column px-2 py-2 align-items-start bg-white rounded">
-                    <span><?= htmlspecialchars($note['title'])?></span>
-                    <span class="hover:bg-dark"><?= htmlspecialchars($note['body'])?></span>
+                    <p class="fs-4 fw-bold text-dark"><?= htmlspecialchars($note['title'])?></p>
+                    <p class="text-dark px-3"><?= htmlspecialchars($note['body'])?></p>
                     <span>Hello</span>
                 </div>
             </div>
